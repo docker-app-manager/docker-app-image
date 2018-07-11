f881ef78ac80945eb6a1d38d04f144e2edba78fb
galatolofederico <galatolo.federico@gmail.com>
2018-07-11 23:14:26 +0200

    Use capabilities vector in dam-init.sh

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM ubuntu:16.04
 
-RUN apt-get update \
- && DEBIAN_FRONTEND=noninteractive apt-get install --yes pulseaudio-utils x11-apps sudo
-
 COPY dam-init.sh /dam-init.sh
 
 RUN chmod +x /dam-init.sh
--- dam-init.sh
+++ dam-init.sh
@@ -1,25 +1,43 @@
-#! /bin/sh
+#! /bin/bash
 
-# ./dam-init.sh USERNAME UID GID
-if [ "$#" -ne 3 ]; then
-    echo "./dam-init.sh <username> <uid> <gid>"
+# ./dam-init.sh <USERNAME> <UID> <GUID> <CAPABILITIES>
+if [ "$#" -ne 4 ]; then
+    echo "./dam-init.sh <username> <uid> <gid> <capabilities>"
     exit 1
 fi
 
-UNAME=$1
-UID=$2
-GID=$3
-useradd -m $UNAME
-usermod  --uid $UID $UNAME
-groupmod --gid $GID $UNAME
 
+DAM_USERNAME=$1
+DAM_UID=$2
+DAM_GID=$3
+IFS=";"; DAM_CAPABILITIES=($4); unset IFS;
+
+apt-get update
+apt-get install sudo
+
+# Create user $DAM_USERNAME with matching host UID and GID
+useradd -m $DAM_USERNAME
+usermod  --uid $DAM_UID $DAM_USERNAME
+groupmod --gid $DAM_GID $DAM_USERNAME
+
+# Enable sudo without asking for password for $DAM_USERNAME
 mkdir -p /etc/sudoers.d
-echo "$UNAME ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$UNAME
-chmod 0440 /etc/sudoers.d/$UNAME
-gpasswd -a $UNAME audio
+echo "$DAM_USERNAME ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$DAM_USERNAME
+chmod 0440 /etc/sudoers.d/$DAM_USERNAME
 
-echo "default-server = unix:/run/user/$UID/pulse/native
-autospawn = no
-daemon-binary = /bin/true
-enable-shm = false
-" > /etc/pulse/client.conf
+if [[ " ${DAM_CAPABILITIES[@]} " =~ " x " ]]; then
+    apt-get install --yes x11-common
+fi
+
+if [[ " ${DAM_CAPABILITIES[@]} " =~ " pulse " ]]; then
+    # Install pulseaudio-utils
+    apt-get install --yes pulseaudio-utils
+    # Add $DAM_USERNAME to audio group
+    gpasswd -a $DAM_USERNAME audio
+    # Create config file
+    echo "default-server = unix:/run/user/$DAM_UID/pulse/native \
+    autospawn = no \
+    daemon-binary = /bin/true \
+    enable-shm = false \
+    " > /etc/pulse/client.conf
+fi
